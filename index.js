a0fcaf52ca39b8013cab155b242cc9e52e7dd99c
Adebayo Moses <adebayomoses597@gmail.com>
2022-10-30 19:51:11 -0500

    fix: bio constants

--- index.js
+++ index.js
@@ -5,13 +5,16 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 
 // app.get('/', (req, res) => res.send('Hello World!'));
+
+const bio = {
+  slackUsername: 'Max',
+  backend: 'true',
+  age: 23,
+  bio: "I'm a software developer with vast experience in building scalable web applications.",
+};
+
 app.get('/', (req, res) => {
-  res.json({
-    slackUsername: 'Max',
-    backend: 'true',
-    age: 23,
-    bio: "I'm a software developer with vast experience in building scalable web applications.",
-  });
+  res.json({});
 });
 const server = app.listen(port, () =>
   console.log(`Task 1 listening on port ${port}!`)
