042c1282602786964f7a67d2bed53375c47c0626
Adebayo Moses <adebayomoses597@gmail.com>
2022-10-30 19:45:29 -0500

    fix: server function

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
+app.use(cors());
 
 // app.get('/', (req, res) => res.send('Hello World!'));
 app.get('/', (req, res) => {
@@ -12,7 +13,9 @@ app.get('/', (req, res) => {
     bio: "I'm a software developer with vast experience in building scalable web applications.",
   });
 });
-app.listen(port, () => console.log(`Task 1 listening on port ${port}!`));
+const server = app.listen(port, () =>
+  console.log(`Task 1 listening on port ${port}!`)
+);
 
 server.setTimeout(10000, (socket) => {
   console.log('Opps timeout buddy.');
